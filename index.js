6dcd3774426e14d6dc9a177741122e82f8f02b03
SuryaNarayanan <87986571+SuryaThillai@users.noreply.github.com>
2024-04-04 15:06:53 +0530

    Update index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+
+     
+
+
+
+
+// document.querySelector('button').addEventListener('click', function(){
+//     var inputValue = document.querySelector('.todos').value;
+    
+//     var container = document.querySelector('.container');
+//     document.querySelector('.task').innerHTML = inputValue;
+//     var duplicate_div = document.querySelector('.list');
+//     container.appendChild(duplicate_div.cloneNode(true));
+// });
