b2c04d9c389dcb897a6712421e659116c871ba33
SuryaNarayanan <87986571+SuryaThillai@users.noreply.github.com>
2024-04-07 11:40:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,63 @@
 
-     
+document.getElementById('btn').addEventListener('click', function(e){
+    var inputValue = document.querySelector('.todos').value;
+    if (inputValue === ''){
+        setError();
+    } else
+        setSuccess();
+        var parent_element = document.querySelector('.container')
+        var div = document.createElement('div');
+        div.classList.add('list');
+        parent_element.appendChild(div);
+    
+        var unchecked_img = document.createElement('img');
+        unchecked_img.classList.add('common');
+        unchecked_img.classList.add('unchecked');
+        div.appendChild(unchecked_img);
+    
+        var p = document.createElement('p');
+        p.innerHTML = inputValue;
+        div.appendChild(p);
+    
+    
+        var cross_img = document.createElement('img');
+        cross_img.classList.add('cross');
+        div.appendChild(cross_img);
 
+        document.querySelector('.todos').value = '';
+
+                
+        document.querySelector('.unchecked').addEventListener('click', () =>{
+            checkorUncheck();
+        })
+});
+
+
+function checkorUncheck() {
+    document.querySelector('.unchecked').classList.add('checked');
+    document.querySelector('.unchecked').classList.remove('unchecked');
+};
+
+function deleteitems(){
+    
+
+}
+
+
+function setError(){
+    var getting_div = document.querySelector('.only');
+    getting_div.classList.add('error');
+    getting_div.innerHTML = 'Enter a task to add';
+}
+
+function setSuccess(){
+    var getting_div = document.querySelector('.only');
+    getting_div.classList.remove('error');
+    getting_div.innerHTML = '';
+    
+    
+}
+  
 
 
 
